1bbe87cede5bb750d0a651ff6409c5457f51595d
Катерина Полякова <katsiaryna.paliakova@gmail.com>
2022-02-05 13:14:16 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int currentBalance = 200;
+        int income = 1070;
+        if (income > 1000){
+            int bonus = income / 100;
+            int totalBalance = currentBalance + bonus;
+            System.out.println("Сумма бонуса:" + bonus);
+            System.out.println("Итоговый счет:" + totalBalance);
+        }
+else {
+    int bonus = 0;
+    int totalBalance = currentBalance + bonus;
+            System.out.println("Сумма бонуса:" + bonus);
+            System.out.println("Итоговый счет:" + totalBalance);
+        }
+    }
+}

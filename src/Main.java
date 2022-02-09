8c44f7a02826a31bb568d565cc93173ba018bda2
Катерина Полякова <katsiaryna.paliakova@gmail.com>
2022-02-09 22:12:18 +0100

    Reformat Code

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,16 @@ import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int currentBalance = 200;
-        int income = 1070;
-        if (income > 1000){
+        int currentBalance = 550;
+        int income = 1010;
+        if (income > 1000) {
             int bonus = income / 100;
             int totalBalance = currentBalance + bonus;
             System.out.println("Сумма бонуса:" + bonus);
             System.out.println("Итоговый счет:" + totalBalance);
-        }
-else {
-    int bonus = 0;
-    int totalBalance = currentBalance + bonus;
+        } else {
+            int bonus = 0;
+            int totalBalance = currentBalance + bonus;
             System.out.println("Сумма бонуса:" + bonus);
             System.out.println("Итоговый счет:" + totalBalance);
         }
